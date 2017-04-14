d129ba767aa958581916515bb1e56990e1915473
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2017-04-14 00:43:52 -0300

    Add LoadSubscribers func

--- subscriber.go
+++ subscriber.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"io/ioutil"
+
+	yaml "gopkg.in/yaml.v2"
+)
 
 type Subscriber struct {
-	Topics []string
+	Topics    []string `yaml:"Topics"`
 }
 
 func NewSubscriber() (*Subscriber, error) {
@@ -13,4 +21,39 @@ func NewSubscriber() (*Subscriber, error) {
 
 func (s *Subscriber) Receiver(topic string) {
 	fmt.Println(topic)
+}
+
+func LoadSubscribers(path string) ([]*Subscriber, error) {
+	if stat, err := os.Stat(path); err != nil {
+		return nil, err
+	} else {
+		if !stat.IsDir() {
+			return nil, fmt.Errorf("Not a directory")
+		}
+	}
+
+	files, err := filepath.Glob(filepath.Join(path, "*.yaml"))
+	if err != nil {
+		return nil, err
+	}
+
+	subscribers := []*Subscriber{}
+
+	for _, file := range files {
+		subscriber, _ := NewSubscriber()
+
+		data, err := ioutil.ReadFile(file)
+		if err != nil {
+			return nil, err
+		}
+
+		err = yaml.Unmarshal(data, subscriber)
+		if err != nil {
+			return nil, err
+		}
+
+		subscribers = append(subscribers, subscriber)
+	}
+
+	return subscribers, nil
 }
